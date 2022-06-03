11054bc6f4c6226193b47aa0b2bbe6cb71043a8d
brunacr <bruna.leitao.pro@gmail.com>
2022-06-03 01:06:42 -0300

    Comunicação com o back-end

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
-import './App.css';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import './App.css';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/home" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> 
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
+      </div>
       <Footer />
     </Router>
-
-  );
+  )
 }
 
 export default App;
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -5,39 +5,39 @@ import './Navbar.css'
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
+            <AppBar position="static" className='menu'>
                 <Toolbar variant="dense">
                     <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
-                            BlogPessoal
+                            BlogdaBru
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                home
+                                Home
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                postagens
+                                Postagens
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                temas
+                                Temas
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                cadastrar tema
+                                Cadastrar Tema
                             </Typography>
                         </Box>
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
                                 <Typography variant="h6" color="inherit">
-                                    logout
+                                    Logout
                                 </Typography>
                             </Box>
                         </Link>
